1de6cad773ba472e3ad2db00f19a86952557440a
wrpoulson <wrpoulson@gmail.com>
2017-08-07 13:52:13 -0400

    Finish props and passing data videos.

--- reactjs-basics/src/app/index.jsx
+++ reactjs-basics/src/app/index.jsx
@@ -6,6 +6,10 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
     render(){
+        var user = {
+            name: "William",
+            hobbies:["dogs", "programming", "general smartassery"]
+        }
         return (
             <div className="container">
                 <div className="row">
@@ -15,7 +19,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Bill"} age={30} user={user}>
+                            <p>This is a paragraph!</p>
+                        </Home>
                     </div>
                 </div>
             </div>
